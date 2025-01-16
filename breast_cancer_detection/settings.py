5851bb904013625bc3362ae775c907cb5ea6ccd4
Angela Chacha <angieschacha@gmail.com>
2025-01-16 13:23:53 +0300

    add login authentication

--- breast_cancer_detection/settings.py
+++ breast_cancer_detection/settings.py
@@ -136,6 +136,9 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+LOGIN_REDIRECT_URL = 'home'  # Redirect to home page after login
+LOGOUT_REDIRECT_URL = 'login'  # Redirect to login page after logout
+
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
--- breast_cancer_detection/urls.py
+++ breast_cancer_detection/urls.py
@@ -20,6 +20,7 @@ from django.urls.conf import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('', include('users.urls')),
     # path('', include('predictions.urls')),
 ]
