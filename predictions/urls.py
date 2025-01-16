19b3fe2f57fcca6c652565a432910a97ce466228
Angela Chacha <angieschacha@gmail.com>
2025-01-16 14:51:30 +0300

    add upload image view

---
+++ predictions/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import upload_mammogram, processing_view, results_view
+
+urlspatterns = [
+    path('upload/', upload_mammogram, name='upload_mammogram'),
+    path('process/<uuid:mammogram_id>/', processing_view, name='process_mammogram'),
+    path('results/<uuid:mammogram_id>/', results_view, name='results'),
+]
--- predictions/views.py
+++ predictions/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import MammogramForm
+from .models import Mammogram
 
-# Create your views here.
+
+def upload_mammogram(request):
+    if request.method == 'POST':
+        form = MammogramForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('')
+    else:
+        form = MammogramForm()
+    return render(request, 'predictions/upload_mammogram.html', {'form': form})
+
+def processing_view(request, mammogram_id):
+    mammogram = Mammogram.objects.get(pk=mammogram_id)
+
+    # process the mammogram
+    
+    return render(request, 'predictions/process_mammogram.html')
+
+def results_view(request, mammogram_id):
+    mammogram = Mammogram.objects.get(pk=mammogram_id)
+
+    # get the results of the mammogram
+
+    return render(request, 'predictions/results.html')
