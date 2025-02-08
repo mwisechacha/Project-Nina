8e4921b277a4aee4b1806ceb4668ebeac1554f15
Angela Chacha <angieschacha@gmail.com>
2025-02-08 11:41:12 +0300

    upload image form preview

--- breast_cancer_detection/settings.py
+++ breast_cancer_detection/settings.py
@@ -155,5 +155,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
+EMAIL_HOST_USER = 'angieschacha@gmail.com'
+EMAIL_HOST_PASSWORD = 'nzid qbfs amlu bbju'
