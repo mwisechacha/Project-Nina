e40703f395e00be5b1f825815b543de7c5378dac
Angela Chacha <angieschacha@gmail.com>
2025-03-08 10:50:56 +0300

    fix image model predictions

--- predictions/predictions.py
+++ predictions/predictions.py
@@ -4,9 +4,14 @@ import torchvision.models as models
 from torchvision.models import ResNet18_Weights
 from torchvision import transforms
 import numpy as np
+import cv2
 from PIL import Image
 from django.db.models import  Count, Q
 from .models import GroundTruth
+
+# def preprocess_ploaded_image(image_path):
+#     image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+#     image = cv2.resize(image, (224, 224), interpolation=cv2.INTER_AREA)
 
 def load_model():
     weights = ResNet18_Weights.IMAGENET1K_V1
@@ -17,7 +22,6 @@ def load_model():
         nn.ReLU(),
         nn.Dropout(0.4),
         nn.Linear(128, 1),
-        nn.Sigmoid()
     )
 
     model_path = 'predictions/model/trained_model_3.pth'
@@ -29,25 +33,39 @@ MODEL = load_model()
 
 LABELS = {0: 'Benign', 1: 'Malignant'}
 
-def predict(image_file):
+def preprocess_image(image_path):
+    image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    image = cv2.resize(image, (224, 224), interpolation=cv2.INTER_AREA)
+
+    if image.ndim == 2:
+        image = np.stack([image] * 3, axis=2)
+
+    print(f"Image shape before conversion to PIL: {image.shape}")
+
+    image = Image.fromarray(image.astype(np.uint8))
+
     transform = transforms.Compose([
-        transforms.Resize((224, 224)),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.5, 0.5, 0.5],
                              std=[0.5, 0.5, 0.5])
     ])
 
-    image = Image.open(image_file)
-
-    if image.mode != 'RGB':
-        image = image.convert('RGB')
-
     image = transform(image).unsqueeze(0)
+    return image
+
+def predict(image_file):
+    image = preprocess_image(image_file)
+
+    print(image.shape)
 
     # inference
     with torch.no_grad():
         outputs = MODEL(image)
-        _, preds = torch.max(outputs, 1)
+        outputs = torch.sigmoid(outputs)
+        preds = (outputs > 0.5).int()
+
+    print(f"Model raw output: {outputs.item()}")
+    print(f"Predicted class (0=Benign, 1=Malignant): {preds.item()}")
 
     prediction_label = LABELS[preds.item()]
 
